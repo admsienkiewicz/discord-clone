37bdc88b876714d7b7bcdfef6b02af314df9c0ad
admsienkiewicz <adm.sienkiewicz@gmail.com>
2022-10-07 15:31:57 +0200

    react-lib div100vh added

--- src/App.js
+++ src/App.js
@@ -4,8 +4,9 @@ import Login from './pages/Login'
 import Main from './pages/Main'
 import Register from './pages/Register'
 import CreateServer from './pages/CreateServer'
-import { UserContext, UserContextProvider } from './context/UserContext'
+import { UserContext } from './context/UserContext'
 import { useContext } from 'react'
+import Div100vh from 'react-div-100vh'
 
 function App() {
     const { currUser } = useContext(UserContext)
@@ -16,23 +17,25 @@ function App() {
         return <Navigate to="/login" />
     }
     return (
-        <div className="App">
-            <BrowserRouter>
-                <Routes>
-                    <Route
-                        path="/"
-                        element={
-                            <RedirectToLogin>
-                                <Main />
-                            </RedirectToLogin>
-                        }
-                    />
-                    <Route path="/login" element={<Login />} />
-                    <Route path="/register" element={<Register />} />
-                    <Route path="/create-server" element={<CreateServer />} />
-                </Routes>
-            </BrowserRouter>
-        </div>
+        <Div100vh>
+            <div className="App">
+                <BrowserRouter>
+                    <Routes>
+                        <Route
+                            path="/"
+                            element={
+                                <RedirectToLogin>
+                                    <Main />
+                                </RedirectToLogin>
+                            }
+                        />
+                        <Route path="/login" element={<Login />} />
+                        <Route path="/register" element={<Register />} />
+                        <Route path="/create-server" element={<CreateServer />} />
+                    </Routes>
+                </BrowserRouter>
+            </div>
+        </Div100vh>
     )
 }
 
