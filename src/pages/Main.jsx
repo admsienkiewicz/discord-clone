946da2cf7c2ce7e001c371a60507bdf78ca429c0
admsienkiewicz <adm.sienkiewicz@gmail.com>
2022-10-07 15:35:57 +0200

    div100vh

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -3,16 +3,17 @@ import Channel from '../components/Channel'
 import Server from '../components/Server'
 import ServerList from '../components/ServerList'
 import { SidebarContextProvider } from '../context/SidebarContext'
+import Div100vh from 'react-div-100vh'
 import './Main.scss'
 
 const Main = () => {
     return (
         <SidebarContextProvider>
-            <div className="Main">
+            <Div100vh className="Main">
                 <ServerList />
                 <Server />
                 <Channel />
-            </div>
+            </Div100vh>
         </SidebarContextProvider>
     )
 }
