0e32c12bf5f88060aa2499b5d94c3e2794bcce7c
admsienkiewicz <adm.sienkiewicz@gmail.com>
2022-10-08 22:29:22 +0200

    message img loading

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -24,13 +24,13 @@ const Message = ({ msgProps }) => {
                     <p className="Message__info--date">{sendDate}</p>
                 </div>
                 <span className="Message__content--text">{contentText}</span>
-                {/* {contentImg && loading && <ReactLoading type={'spin'} />} */}
+                {contentImg && loading && <ReactLoading type={'spin'} />}
                 {contentImg && (
                     <img
                         src={contentImg}
                         className="Message__content--img"
-                        // style={loading ? { display: 'none' } : {}}
-                        // onLoad={() => setLoading(false)}
+                        style={loading ? { display: 'none' } : {}}
+                        onLoad={() => setLoading(false)}
                         alt="contet img"
                     />
                 )}
